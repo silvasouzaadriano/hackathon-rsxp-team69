137e6b6d7a276db6d9e69a0ecb3547ce352d0ddc
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-11-26 17:25:34 -0300

    Animation Main Page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Content, FrameLeft, FrameRight } from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <h1>Você já sabe qual profissão escolher?</h1>
-      <h1>Já pensou em tecnologia da informação?</h1>
-      <h1>Você já ouviu falar em linguagem de programação?</h1>
-      <h1>Clique nos links acima e veja onde a programação pode lhe levar</h1>
+      <Content>
+        <p>
+          <FrameLeft>
+            <h1>Você já sabe qual profissão escolher?</h1>
+          </FrameLeft>
+        </p>
+        <p>
+          <FrameRight>
+            <h1>Já pensou em tecnologia da informação?</h1>
+          </FrameRight>
+        </p>
+        <p>
+          <FrameLeft>
+            <h1>Você já ouviu falar em linguagem de programação?</h1>
+          </FrameLeft>
+        </p>
+        <p>
+          <FrameRight>
+            <h1>
+              Clique nos links acima e veja onde a programação pode lhe levar
+            </h1>
+          </FrameRight>
+        </p>
+      </Content>
     </Container>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,42 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.div`
   margin-top: 20px;
   margin-left: 20px;
+`;
+
+export const Content = styled.div`
+  font-size: 1em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const frameleft = keyframes`
+  from {
+    transform: translate3d(-100%,0,0);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+export const FrameLeft = styled.div`
+  display: inline-block;
+  animation: ${frameleft} 6s linear;
+`;
+
+const frameright = keyframes`
+  from {
+    transform: translate3d(100%,0,0);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+export const FrameRight = styled.div`
+  display: inline-block;
+  animation: ${frameright} 6s linear;
 `;
